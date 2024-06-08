9bd5a9023c7b5beb06a5ff234bd0f2e649e8fbf3
JonathanMabiala <62263731+JonathanMabiala@users.noreply.github.com>
2024-06-08 15:08:20 -0400

    new configs and top tabs added

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -6,15 +6,9 @@ const _layout = () => {
   return (
     <Stack>
       <Stack.Screen
-        name="index"
+        name="list"
         options={{
-          title: "Orders",
-        }}
-      />
-      <Stack.Screen
-        name="[id]"
-        options={{
-          title: "Order Details",
+          headerShown: false,
         }}
       />
     </Stack>
